a4f491bb9236d9f5311ca9f0c5e2eacc08b700d3
Farid Zakaria <farid.m.zakaria@gmail.com>
2022-02-17 20:18:14 -0800

    Added small test

--- src/main/scala/codec/Base64.scala
+++ src/main/scala/codec/Base64.scala
@@ -2,12 +2,28 @@ package codec
 
 import chisel3._
 
-case class Base64Params() {
+case class Base64Params(val sizeInBytes: Int) {
+    require(sizeInBytes > 0)
 
+    // We need to divide by 3 since 4 digits can represent 3 bytes
+    // we do ceiling to account for the padding we need to generate
+    // ref: https://stackoverflow.com/a/49633849/143733
+    val paddedLength : Int = 4 * Math.ceil(sizeInBytes / 3.0).toInt
+
+    val base64chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 }
 
+
+
 class Base64(val p: Base64Params) extends Module {
+
+    def Byte() = UInt(8.W)
+
     val io = IO(new Bundle {
-        
+        val input = Input(Vec(p.sizeInBytes, Byte()))
+        val output = Output(Vec(p.paddedLength, Byte()))
     })
+
+
+    io.output := DontCare
 }
--- src/test/scala/codec/Base64Test.scala
+++ src/test/scala/codec/Base64Test.scala
@@ -3,15 +3,28 @@ package codec
 import chisel3._
 import chiseltest._
 import org.scalatest.flatspec.AnyFlatSpec
+import java.nio.charset.Charset;
 
 class Base64Test extends AnyFlatSpec with ChiselScalatestTester {
   behavior of "Base64"
 
+  // Check if a given string contains only ASCII characters
+  // ref: https://stackoverflow.com/a/3585247/143733
+  def isAscii(str: String): Boolean = {
+    return Charset.forName("US-ASCII").newEncoder().canEncode(str);
+  }
 
-  it should "do nothing" in {
-    val params = Base64Params()
+  it should "do a simple encode" in {
+    val str = "Many hands make light work."
+    val params = Base64Params(str.length)
+
+    // Note: This technically just maps a single character to a byte
+    // I don't think this is technically correct since it can be a base16 ?
     test(new Base64(params)) { dut =>
-        // do nothing
+        for ( (c, i) <- str.zipWithIndex) {
+            dut.io.input(i).poke(c.toByte.U(8.W))
+        }
+        
     }
   }
 }
