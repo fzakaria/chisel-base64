672e7acb0b43420b449c32e1ece6d721a1cb2233
Farid Zakaria <farid.m.zakaria@gmail.com>
2022-02-19 19:52:39 -0800

    Fixed the code. It was not expanding the counter addition and causing it to wrap:

--- src/main/scala/codec/Base64.scala
+++ src/main/scala/codec/Base64.scala
@@ -3,65 +3,88 @@ package codec
 import chisel3._
 import chisel3.util._
 
-case class Base64Params(val sizeInBytes: Int) {
-    require(sizeInBytes > 0)
-
-    // We need to divide by 3 since 4 digits can represent 3 bytes
-    // we do ceiling to account for the padding we need to generate
-    // ref: https://stackoverflow.com/a/49633849/143733
-    val paddedLength : Int = 4 * Math.ceil(sizeInBytes / 3.0).toInt
-
-    // TODO: Make this a parameterized input
-    // make sure to assert that the input is 64 characters long.
-    val base64Chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+object Base64Params {
+  val DEFAULT_BASE_CHARS =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
 }
 
-
+case class Base64Params(val sizeInBytes: Int, val base64Chars: String) {
+  require(sizeInBytes > 0)
+  require(base64Chars.length == 64)
+  // We need to divide by 3 since 4 digits can represent 3 bytes
+  // we do ceiling to account for the padding we need to generate
+  // ref: https://stackoverflow.com/a/49633849/143733
+  val paddedLength: Int = 4 * Math.ceil(sizeInBytes / 3.0).toInt
+}
 
 class Base64(val p: Base64Params) extends Module {
 
-    def Byte() = UInt(8.W)
+  def Byte() = UInt(8.W)
 
-    val io = IO(new Bundle {
-        val input = Input(Vec(p.sizeInBytes, Byte()))
-        val output = Valid(Vec(p.paddedLength, Byte()))
-    })
+  val io = IO(new Bundle {
+    val input = Input(Vec(p.sizeInBytes, Byte()))
+    val output = Valid(Vec(p.paddedLength, Byte()))
+  })
 
-    // Helpful method to read an index (byte) off a vector.
-    // It simply checks if the index is passed the length and returns
-    // a 0 (null byte).
-    def read_byte(index: UInt, vec: Vec[UInt]): UInt = {
-        val byte = Wire(Byte())
-        when(index < vec.length.U) {
-            byte := vec(index)
-        }.otherwise {
-            byte := 0.U(8.W)
-        }
-        return byte
+  // Helpful method to read an index (byte) off a vector.
+  // It simply checks if the index is passed the length and returns
+  // a 0 (null byte).
+  def read_byte(index: UInt, vec: Vec[UInt]): UInt = {
+    val byte = Wire(Byte())
+    when(index < vec.length.U) {
+      byte := vec(index)
+    }.otherwise {
+      byte := 0.U(8.W)
     }
+    return byte
+  }
 
-    val mapping = VecInit(p.base64Chars.map(_.toByte.U(8.W)))
+  val mapping = VecInit(p.base64Chars.map(_.toByte.U(8.W)))
 
-    // Algorithm:
-    // Take 3 bytes from input, divide them into four 6bit value
-    // each 6bit value mapps index-0 to a base64 character
-    val (index, wrapped) = Counter(0 until p.sizeInBytes by 3, true.B, false.B)
-    val (outputIndex, outputIndexWrapped) = Counter(0 until p.paddedLength by 4, true.B, false.B)
+  // Algorithm:
+  // Take 3 bytes from input, divide them into four 6bit value
+  // each 6bit value mapps index-0 to a base64 character
+  val (index, wrapped) = Counter(0 until p.sizeInBytes by 3, true.B, false.B)
+  val (outputIndex, outputIndexWrapped) =
+    Counter(0 until p.paddedLength by 4, true.B, false.B)
 
-    // Note: bit field access is inclusive (0, 7) since it's a byte
-    val byte1 = mapping(read_byte(index, io.input)(7, 2))
-    val byte2 = mapping(Cat(read_byte(index, io.input)(1, 0), read_byte(index + 1.U, io.input)(7, 4)))
-    val byte3 = mapping(Cat(read_byte(index + 1.U, io.input)(3, 0), read_byte(index + 2.U, io.input)(7, 6)))
-    val byte4 = mapping(read_byte(index + 2.U, io.input)(5, 0))
+  // Note: bit field access is inclusive (0, 7) since it's a byte
+  val byte1 = mapping(read_byte(index, io.input)(7, 2))
+  // Note: BE SUPER MINDFUL about when you increment the index since it will wrap.
+  //       You have to use the addition that expands the width of the UInt
+  //       So much frustration!
+  val byte2 = mapping(
+    Cat(
+      read_byte(index, io.input)(1, 0),
+      read_byte(index +& 1.U, io.input)(7, 4)
+    )
+  )
+  val byte3 = mapping(
+    Cat(
+      read_byte(index +& 1.U, io.input)(3, 0),
+      read_byte(index +& 2.U, io.input)(7, 6)
+    )
+  )
+  val byte4 = mapping(read_byte(index +& 2.U, io.input)(5, 0))
 
-    val equalsChar = '='.toByte.U(8.W)
+  val equalsChar = '='.toByte.U(8.W)
 
-    io.output.valid := true.B
-    io.output.bits := DontCare
-    io.output.bits(outputIndex) := byte1
-    io.output.bits(outputIndex + 1.U) := byte2
-    io.output.bits(outputIndex + 2.U) := Mux(index + 1.U >= io.input.length.U, equalsChar, byte3)
-    io.output.bits(outputIndex + 3.U) := Mux(index + 2.U >= io.input.length.U, equalsChar, byte4)
+  io.output.valid := true.B
+  io.output.bits := DontCare
+  io.output.bits(outputIndex) := byte1
+  io.output.bits(outputIndex + 1.U) := byte2
+  io.output.bits(outputIndex + 2.U) := Mux(
+    index +& 1.U >= io.input.length.U,
+    equalsChar,
+    byte3
+  )
+  io.output.bits(outputIndex + 3.U) := Mux(
+    index +& 2.U >= io.input.length.U,
+    equalsChar,
+    byte4
+  )
 
-    printf(p"index: ${index} output: ${io.output.bits}\n")
+  printf(
+    p"index: ${index} output: ${io.output.bits}\n"
+  )
 }
--- src/test/scala/codec/Base64Test.scala
+++ src/test/scala/codec/Base64Test.scala
@@ -7,7 +7,7 @@ import java.nio.charset.Charset;
 import org.scalacheck.Gen
 import org.scalacheck.Prop.forAll
 
-class Base64Test extends AnyFlatSpec with ChiselScalatestTester{
+class Base64Test extends AnyFlatSpec with ChiselScalatestTester {
   behavior of "Base64"
 
   // Check if a given string contains only ASCII characters
@@ -18,101 +18,101 @@ class Base64Test extends AnyFlatSpec with ChiselScalatestTester{
 
   it should "do a simple encode of exactly 3 bytes" in {
     val str = "lol"
-    val params = Base64Params(str.length)
+    val params = Base64Params(str.length, Base64Params.DEFAULT_BASE_CHARS)
     val encoded = java.util.Base64.getEncoder.encode(str.getBytes())
     // Note: This technically just maps a single character to a byte
     // I don't think this is technically correct since it can be a base16 ?
     test(new Base64(params)) { dut =>
-        for ( (c, i) <- str.zipWithIndex) {
-            dut.io.input(i).poke(c.toByte.U(8.W))
-        }
+      for ((c, i) <- str.zipWithIndex) {
+        dut.io.input(i).poke(c.toByte.U(8.W))
+      }
 
-        dut.io.output.valid.expect(true.B)
-        for ( (c, i) <- encoded.zipWithIndex) {
-            dut.io.output.bits(i).expect(c.U(8.W))
-        } 
+      dut.io.output.valid.expect(true.B)
+      for ((c, i) <- encoded.zipWithIndex) {
+        dut.io.output.bits(i).expect(c.U(8.W))
+      }
     }
   }
 
   it should "do a simple encode of exactly 1 bytes (2-padding)" in {
     val str = "M"
-    val params = Base64Params(str.length)
+    val params = Base64Params(str.length, Base64Params.DEFAULT_BASE_CHARS)
     val encoded = java.util.Base64.getEncoder.encode(str.getBytes())
     // Note: This technically just maps a single character to a byte
     // I don't think this is technically correct since it can be a base16 ?
     test(new Base64(params)) { dut =>
-        for ( (c, i) <- str.zipWithIndex) {
-            dut.io.input(i).poke(c.toByte.U(8.W))
-        }
+      for ((c, i) <- str.zipWithIndex) {
+        dut.io.input(i).poke(c.toByte.U(8.W))
+      }
 
-        dut.io.output.valid.expect(true.B)
-        for ( (c, i) <- encoded.zipWithIndex) {
-            dut.io.output.bits(i).expect(c.U(8.W))
-        } 
+      dut.io.output.valid.expect(true.B)
+      for ((c, i) <- encoded.zipWithIndex) {
+        dut.io.output.bits(i).expect(c.U(8.W))
+      }
     }
   }
 
   it should "do a simple encode of exactly 2 bytes (1-padding)" in {
     val str = "Ma"
-    val params = Base64Params(str.length)
+    val params = Base64Params(str.length, Base64Params.DEFAULT_BASE_CHARS)
     val encoded = java.util.Base64.getEncoder.encode(str.getBytes())
     // Note: This technically just maps a single character to a byte
     // I don't think this is technically correct since it can be a base16 ?
     test(new Base64(params)) { dut =>
-        for ( (c, i) <- str.zipWithIndex) {
-            dut.io.input(i).poke(c.toByte.U(8.W))
-        }
+      for ((c, i) <- str.zipWithIndex) {
+        dut.io.input(i).poke(c.toByte.U(8.W))
+      }
 
-        dut.io.output.valid.expect(true.B)
-        for ( (c, i) <- encoded.zipWithIndex) {
-            dut.io.output.bits(i).expect(c.U(8.W))
-        } 
+      dut.io.output.valid.expect(true.B)
+      for ((c, i) <- encoded.zipWithIndex) {
+        dut.io.output.bits(i).expect(c.U(8.W))
+      }
     }
   }
 
-  it should "do a simple encode of exactly 4 bytes (2-padding)" in {
-    val str = "Many"
-    val params = Base64Params(str.length)
+  it should "do a simple encode of exactly 5 bytes (1-padding)" in {
+    val str = "Many "
+    val params = Base64Params(str.length, Base64Params.DEFAULT_BASE_CHARS)
     val encoded = java.util.Base64.getEncoder.encode(str.getBytes())
     // Note: This technically just maps a single character to a byte
     // I don't think this is technically correct since it can be a base16 ?
-    test(new Base64(params)) { dut =>
-        for ( (c, i) <- str.zipWithIndex) {
-            dut.io.input(i).poke(c.toByte.U(8.W))
-        }
+    test(new Base64(params)).withAnnotations(Seq(WriteVcdAnnotation)) { dut =>
+      for ((c, i) <- str.zipWithIndex) {
+        dut.io.input(i).poke(c.toByte.U(8.W))
+      }
 
-        dut.io.output.valid.expect(true.B)
-        for ( i <- 0 until encoded.length by 4) {
-            dut.io.output.bits(i).expect(encoded(i).U(8.W))
-            dut.io.output.bits(i + 1).expect(encoded(i + 1).U(8.W))
-            dut.io.output.bits(i + 2).expect(encoded(i + 2).U(8.W))
-            dut.io.output.bits(i + 3).expect(encoded(i + 3).U(8.W))
-            
-            dut.clock.step()
-        }
+      dut.io.output.valid.expect(true.B)
+      for (i <- 0 until encoded.length by 4) {
+        dut.io.output.bits(i).expect(encoded(i).U(8.W))
+        dut.io.output.bits(i + 1).expect(encoded(i + 1).U(8.W))
+        dut.io.output.bits(i + 2).expect(encoded(i + 2).U(8.W))
+        dut.io.output.bits(i + 3).expect(encoded(i + 3).U(8.W))
+
+        dut.clock.step()
+      }
     }
   }
 
   it should "do a simple encode of larger than 3 bytes divisible by 3" in {
     val str = "Many hands make light work."
-    val params = Base64Params(str.length)
+    val params = Base64Params(str.length, Base64Params.DEFAULT_BASE_CHARS)
     val encoded = java.util.Base64.getEncoder.encode(str.getBytes())
     // Note: This technically just maps a single character to a byte
     // I don't think this is technically correct since it can be a base16 ?
     test(new Base64(params)) { dut =>
-        for ( (c, i) <- str.zipWithIndex) {
-            dut.io.input(i).poke(c.toByte.U(8.W))
-        }
+      for ((c, i) <- str.zipWithIndex) {
+        dut.io.input(i).poke(c.toByte.U(8.W))
+      }
 
-        dut.io.output.valid.expect(true.B)
-        for ( i <- 0 until encoded.length by 4) {
-            dut.io.output.bits(i).expect(encoded(i).U(8.W))
-            dut.io.output.bits(i + 1).expect(encoded(i + 1).U(8.W))
-            dut.io.output.bits(i + 2).expect(encoded(i + 2).U(8.W))
-            dut.io.output.bits(i + 3).expect(encoded(i + 3).U(8.W))
-            
-            dut.clock.step()
-        } 
+      dut.io.output.valid.expect(true.B)
+      for (i <- 0 until encoded.length by 4) {
+        dut.io.output.bits(i).expect(encoded(i).U(8.W))
+        dut.io.output.bits(i + 1).expect(encoded(i + 1).U(8.W))
+        dut.io.output.bits(i + 2).expect(encoded(i + 2).U(8.W))
+        dut.io.output.bits(i + 3).expect(encoded(i + 3).U(8.W))
+
+        dut.clock.step()
+      }
     }
   }
 }
