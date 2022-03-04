c58e3dc5f317719394657ef98f833ebe23be3f43
Connor Masterson <ccmaster@ucsc.edu>
2022-03-03 19:04:59 -0800

    new tests for new module

--- src/test/scala/codec/Base64Test.scala
+++ src/test/scala/codec/Base64Test.scala
@@ -7,6 +7,62 @@ import java.nio.charset.Charset;
 import org.scalacheck.Gen
 import org.scalacheck.Prop.forAll
 
+class WrapperInterface {
+  def Base64WrapperTest(dut: Base64Wrapper, inputStr: String, bytesPerCycle: Int): Unit = {
+    val params = Base64WrapperParams(bytesPerCycle, Base64Params.DEFAULT_BASE_CHARS)
+    var allInputs = inputStr
+    val numInputs = Math.ceil(allInputs.length / params.bytesPerCycle.toFloat)
+
+    for (i <- 0 until numInputs.toInt) {
+      var currentInputs = allInputs.take(params.bytesPerCycle)
+      allInputs = allInputs.drop(params.bytesPerCycle)
+      while(currentInputs.size != params.bytesPerCycle) {
+        currentInputs = currentInputs.concat("=")
+      }
+
+      val encoded = java.util.Base64.getEncoder.encode(currentInputs.toString.getBytes())
+
+      for ((c, i) <- currentInputs.zipWithIndex) {
+        dut.io.input.bits(i).poke(c)
+      }
+      dut.io.input.valid.poke(true.B)
+      while ((dut.io.output.valid.peek().litValue() == 0)) dut.clock.step()
+      for ((c, i) <- encoded.zipWithIndex) {
+        dut.io.output.bits(i).expect(c.U(8.W))
+      }
+    }
+  }
+}
+
+class Base64WrapperTest extends AnyFlatSpec with ChiselScalatestTester {
+  behavior of "Base64Wrapper"
+
+  it should "stream in bytesPerCycle bytes at a time to a Base64 module" in {
+
+    // some sample input strings
+    val lol = "lol"
+    val school = "University of California Santa Cruz"
+    val marquez = "Many years later as he faced the firing squad colonel Aureliano Buendia " +
+                  "was to remember that distant afternoon when his father took him to discover ice"
+    val herbert = "He felt the bubble lift him, felt it break and the dust whirlpool engulf him, " +
+                  "dragging him down into cool darkness. For a moment, the sensation of coolness " +
+                  "and the moisture were blessed relief. Then, as his planet killed him, it occurred " +
+                  "to Kynes that his father and all the other scientists were wrong, that the most " +
+                  "persistent principles of the universe were accident and error. " +
+                  "Even the hawks could appreciate these facts."
+
+    val params = Base64WrapperParams(3, Base64Params.DEFAULT_BASE_CHARS)
+
+    val t = new WrapperInterface
+
+    test(new Base64Wrapper(params)) { dut =>
+      t.Base64WrapperTest(dut, lol, 3)
+      t.Base64WrapperTest(dut, school, 3)
+      t.Base64WrapperTest(dut, marquez, 3)
+    }
+  }
+}
+
 class Base64Test extends AnyFlatSpec with ChiselScalatestTester {
   behavior of "Base64"
 
